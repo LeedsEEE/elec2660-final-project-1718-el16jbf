5fa0b6405801850d9f2edf311e6f04b5d931df25
Joshua Fincher [el16jbf] <el16jbf@eentpc199.leeds.ac.uk>
2017-11-07 12:48:34 +0000

    Initial Commit

---
+++ Project - Pub Crawl Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project - Pub Crawl Tracker
+//
+//  Created by Joshua Fincher [el16jbf] on 07/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project - Pub Crawl Tracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project - Pub Crawl Tracker
+//
+//  Created by Joshua Fincher [el16jbf] on 07/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
